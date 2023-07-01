86833ab61eaea5812ee29b77e88aa3f9b53f463b
Drishit Mitra <drishitmitra@gmail.com>
2023-07-01 22:25:26 +0530

    Update prisma to 4.16.2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const prismaClient = require("@prisma/client");
 async function testDecimal() {
   await prisma.user.create({ data: { id: "foo", decimal: 0 } });
 
-  const decimal = new prismaClient.Prisma.Decimal("45125862532574256.2984");
+  const testNumber = "45125862532574256.2984";
+  const decimal = new prismaClient.Prisma.Decimal(testNumber);
   await prisma.user.upsert({
     create: {
       id: "foo",
@@ -17,7 +18,8 @@ async function testDecimal() {
   });
 
   const user = await prisma.user.findUnique({ where: { id: "foo" } });
-  console.log(user?.decimal);
+  console.log("expected:", testNumber);
+  console.log("actual:", user?.decimal);
 }
 
 testDecimal();
